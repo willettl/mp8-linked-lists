e106c3fd3b8593dcef96b8812519bbfb48439cf8
lucas <willettl@grinnell.edu>
2024-04-17 22:37:58 -0500

    add SDLLExp

--- src/SDLLExpt.java
+++ src/SDLLExpt.java
@@ -1,14 +1,20 @@
 import java.io.PrintWriter;
 
 /**
+ * Tests of the SimpleDLL class.
+ *
+ * @author Samuel A. Rebelsky
+ */
+
+/**
  * Some simple experiments with SimpleDLLs
  */
 public class SDLLExpt {
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
-    SimpleListExpt.expt1(pen, new SimpleDLL<String>());
-    SimpleListExpt.expt2(pen, new SimpleDLL<String>());
-    SimpleListExpt.expt3(pen, new SimpleDLL<String>());
+    SimpleListExpt.expt1(pen, new SimpleCDLL<String>());
+    SimpleListExpt.expt2(pen, new SimpleCDLL<String>());
+    SimpleListExpt.expt3(pen, new SimpleCDLL<String>());
     // SimpleListExpt.expt4(pen, new SimpleDLL<String>(), 3);
   } // main(String[]
 } // SDLLExpt
